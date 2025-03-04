a075b79ac07337da23d0f6e2cb5b8e098d5a3a22
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-03-04 10:57:03 +0530

    [Aditya] Add . Sum of Two Dice Number

---
+++ sequence_practice_problem/AddTwoDiceNumber.js
@@ -0,0 +1,6 @@
+// generate number between 1 to 6
+let firstDiceNumber = Math.floor(Math.random() * 6+1);
+let secondDiceNumber = Math.floor(Math.random() * 6+1);
+// Add two dice Number
+let result = firstDiceNumber+secondDiceNumber;
+console.log(result);
