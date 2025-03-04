45ae2ee87bc9b2dd3243077fb9a91afb3b691252
AdityaRaj00007 <rajgupta49102@gmail.com>
2025-03-04 13:53:36 +0530

    [Aditya] Add . Program to Display Unit of Number

---
+++ case_statement/DisplayUnit.js
@@ -0,0 +1,17 @@
+let number = Number(process.argv[2]);
+switch (number) {
+    case 1:
+        console.log("Unit");
+        break;
+    case 10:
+        console.log("Ten");
+        break;
+    case 100:
+        console.log("Hundred");
+        break;
+    case 1000:
+        console.log("Thousand");
+        break;
+    default:
+        console.log("input 1, 10, 100 or 1000");
+}
